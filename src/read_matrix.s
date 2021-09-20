fdec8c0d02ac91d6a55d8d2a33dc219d869f5d64
Yujia Qiao <rapiz3142@gmail.com>
2021-09-20 19:28:06 +0800

    read_matrix

--- src/read_matrix.s
+++ src/read_matrix.s
@@ -26,16 +26,106 @@
 read_matrix:
 
     # Prologue
-	
+	addi sp, sp, -24
+    sw s0, 0(sp)
+    sw s1, 4(sp)
+    sw s2, 8(sp)
+    sw s3, 12(sp)
+    sw s4, 16(sp)
+    sw ra, 20(sp)
+
+    mv s0 a0
+    mv s1 a1
+    mv s2 a2
+
+    mv a1 s0
+    li a2 0
+    jal ra fopen
+    mv s4 a0        # f = fopen(s0, RDONLY)
+
+    # handle error 
+    li t0, 1
+    bge a0, t0, fopen_good
+    li a1 50
+    jal ra exit2
+fopen_good:
+    # mv a1 s4
+    # jal ra print_int
+
+    mv a1 s4
+    mv a2 s1
+    li a3 4
+    jal ra fread    # fread(f, s1, 4)
+
+    # handle error
+    li t0 4
+    bne a0 t0 fread_bad
+
+    mv a1 s4
+    mv a2 s2
+    li a3 4
+    jal ra fread    # fread(f, s2, 4)    
 
 
+    # handle error
+    li t0 4
+    bne a0 t0 fread_bad
 
 
+    lw t1, 0(s1)
+    lw t2, 0(s2)
+    mul a0, t1, t2
+    li t0 4
+    mul a0, a0, t0
+    jal ra malloc
+    mv s3 a0       # buf = malloc(s1*s2)
 
+    # print row, col
+    # lw a1, 0(s1)
+    # jal ra print_int
+    # lw a1, 0(s2)
+    # jal ra print_int
 
+    mv a1 s4
+    mv a2 s3
+    lw t1, 0(s1)
+    lw t2, 0(s2)
+    mul a3, t1, t2
+    li t0 4
+    mul a3, a3, t0
+    jal ra fread # fread(f, buf, (*s1)*(*s2)*4)
 
+    # handle error
+    lw t1, 0(s1)
+    lw t2, 0(s2)
+    mul t3, t1, t2
+    li t0 4
+    mul t3, t3, t0
+    bne a0 t3 fread_bad
+
+    mv a1 s4
+    jal fclose # fclose(fd)
+
+    mv a0 s3
 
     # Epilogue
-
+    lw s0, 0(sp)
+    lw s1, 4(sp)
+    lw s2, 8(sp)
+    lw s3, 12(sp)
+    lw s4, 16(sp)
+    lw ra, 20(sp)
+	addi sp, sp, 24
 
     ret
+
+fread_bad:
+    li a1 51
+    jal ra exit2
+fclose_bad:
+    li a1 52
+    jal ra exit2
+
+bad:
+    li a1 123
+    jal ra exit2
--- tests/assembly/test_read_matrix.s
+++ tests/assembly/test_read_matrix.s
@@ -7,11 +7,20 @@ file_path: .asciiz "inputs/test_read_matrix/test_input.bin"
 .text
 main:
     # Read matrix into memory
+    li a0 8
+    jal malloc
+    mv s0 a0            # s0 = malloc(8)
 
-
+    la a0 file_path
+    mv a1 s0
+    addi a2, s0, 4
+    jal read_matrix     # s1 = read_matrix(file_path, s0, s0+4)
+    mv s1 a0
 
     # Print out elements of matrix
-
-
+    lw a1, 0(s0)
+    lw a2, 4(s0)
+    jal print_int_array
 
     # Terminate the program
+    jal exit
