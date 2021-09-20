25db7cdfa6c559cb923a3438e6a9f89990328546
Yujia Qiao <rapiz3142@gmail.com>
2021-09-20 15:06:26 +0800

    relu

--- src/relu.s
+++ src/relu.s
@@ -15,9 +15,19 @@
 relu:
     # Prologue
 
+    # body
+    add t0, x0, x0 # counter
 
 loop_start:
-    
+    beq t0, a1, loop_end    # if i == n, break loop
+    addi t1, x0, 4          # prepare immdiate 4
+    mul t1, t1, t0          # get address offset
+    add t1, t1, a0          # get element address
+    lw t2, 0(t1)            # load the element
+    bge t2, x0, pos
+    add, t2, x0, x0
+pos:
+    sw t2, 0(t1)
 
 
 
@@ -26,6 +36,8 @@ loop_start:
 
 
 loop_continue:
+    addi t0, t0, 1
+    jal x0, loop_start
 
 
 
